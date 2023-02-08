275f004fead996b0817b63799ad591f8d20e6898
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-02-08 17:46:32 -0300

    Background image and a divider added to RegisterScreen.kt

--- app/src/main/java/com/example/teayudaapp/ui/composable/registerscreen/RegisterScreen.kt
+++ app/src/main/java/com/example/teayudaapp/ui/composable/registerscreen/RegisterScreen.kt
@@ -1,6 +1,7 @@
 package com.example.teayudaapp.ui.composable.registerscreen
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,16 +9,15 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Facebook
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -142,6 +142,11 @@ private fun ContinueButton() {
             text = "Continuar",
             style = MaterialTheme.typography.body1,
             color = MaterialTheme.colors.primary
+        )
+        Icon(
+            imageVector = Icons.Default.KeyboardArrowRight,
+            contentDescription = "Continue action",
+            tint = MaterialTheme.colors.primary
         )
     }
 }
@@ -304,14 +309,27 @@ private fun TittleView() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(174.dp)
-            .background(MaterialTheme.colors.primary),
+            .height(174.dp),
         Alignment.CenterStart,
     ) {
+        Image(
+            painterResource(id = R.drawable.light_blue_register),
+            contentDescription = "Title blue image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(800.dp)
+        )
         Text(
             text = stringResource(id = R.string.app_tittle),
             style = MaterialTheme.typography.h2,
             color = MaterialTheme.colors.onSecondary
+        )
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(16.dp)
+                .absoluteOffset(y = 46.dp)
+                .rotate(175f)
+        , color = MaterialTheme.colors.background
         )
     }
 }
