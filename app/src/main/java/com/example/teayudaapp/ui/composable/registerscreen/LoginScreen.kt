e50a01b802944c1049664bbaea21138c1b98aabf
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-02-10 10:58:05 -0300

    String data for register added to strings.xml

---
+++ app/src/main/java/com/example/teayudaapp/ui/composable/registerscreen/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.teayudaapp.ui.composable.registerscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(){
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LoginScreenPreview(){
+    LoginScreen()
+}
--- app/src/main/java/com/example/teayudaapp/ui/composable/registerscreen/RegisterScreen.kt
+++ app/src/main/java/com/example/teayudaapp/ui/composable/registerscreen/RegisterScreen.kt
@@ -1,34 +1,15 @@
 package com.example.teayudaapp.ui.composable.registerscreen
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.example.teayudaapp.R
 import com.example.teayudaapp.ui.composable.registerscreen.components.*
 
 
@@ -77,9 +58,10 @@ fun RegisterScreen(
         }
 
         item {
-            Divider(Modifier
-                .height(2.dp)
-                .padding(start = padding, end = padding))
+            Divider(
+                Modifier
+                    .height(2.dp)
+                    .padding(start = padding, end = padding))
             Spacer(modifier = Modifier.height(21.67.dp))
         }
 
