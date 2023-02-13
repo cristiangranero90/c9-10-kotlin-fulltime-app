9001f9466fb1c14cb991e6d8c0322497c74e7563
RubenRojasR <71536870+RubenRojasR@users.noreply.github.com>
2023-02-13 13:44:02 -0600

    Add Data access logic project

---
+++ BackEnd/TEA.Dal/DalFactory.cs
@@ -0,0 +1,15 @@
+﻿using TEA.Dal.EFC;
+using TEA.Services;
+
+namespace TEA.Dal
+{
+    public class DalFactory
+    {
+        public static IRepository GetRepository(string type) =>
+            type switch
+            {
+                "EFC" => new ApplicationDbContext(),
+                _ => throw (new NullReferenceException(nameof(type)))
+            };
+    }
+}
