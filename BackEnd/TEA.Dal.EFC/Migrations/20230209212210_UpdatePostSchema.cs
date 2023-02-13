6fb93acf541783c2a8a31802585c04cd1507a25a
RubenRojasR <71536870+RubenRojasR@users.noreply.github.com>
2023-02-13 13:46:52 -0600

    Add Entity Framework core project

---
+++ BackEnd/TEA.Dal.EFC/Migrations/20230209212210_UpdatePostSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TEA.Dal.EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePostSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Coments",
+                table: "Post",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coments",
+                table: "Post");
+        }
+    }
+}
---
+++ BackEnd/TEA.Dal.EFC/TEAContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TEA.Helpers;
+using TEA.Models.Entities;
+
+namespace TEA.Dal.EFC
+{
+    public partial class ApplicationDbContext : IdentityDbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("TestConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        #region tables 
+        public DbSet<User> User { get; set; }   
+        public DbSet<Post> Post { get; set; }   
+        public DbSet<PostGallery> PostGallery { get; set; }   
+        public DbSet<Coment> Coment { get; set; }   
+        public DbSet<Reaction> Reaction { get; set; }   
+        public DbSet<Message> Message { get; set; }   
+        public DbSet<Friendship> Friendship { get; set; }   
+
+        #endregion
+    }
+}
