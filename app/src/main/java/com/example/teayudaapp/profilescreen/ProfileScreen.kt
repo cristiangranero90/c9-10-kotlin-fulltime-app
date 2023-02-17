955ebdb5bfdf86411e622c330d3b02adc531e118
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-02-17 20:35:15 -0300

    Profile screen with components added to the project

--- app/src/main/java/com/example/teayudaapp/profilescreen/ProfileScreen.kt
+++ app/src/main/java/com/example/teayudaapp/profilescreen/ProfileScreen.kt
@@ -1,10 +1,37 @@
 package com.example.teayudaapp.profilescreen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.teayudaapp.profilescreen.components.ProfileTopBar
 
 @Composable
 fun ProfileScreen(){
+
+    val scaffoldState = rememberScaffoldState()
+    
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { ProfileTopBar(buttonEdit = { /*TODO*/ } )},
+        scaffoldState = scaffoldState
+
+    ) { paddingValues ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ){
+
+        }
+    }
+
+
 
 }
 
