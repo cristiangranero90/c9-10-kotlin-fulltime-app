ef504bb0575240eee78fb438d11b16cd45e16480
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-02-17 17:46:40 -0300

    Register-login buttons in register screen now have the correct button image from Figma UI

--- app/src/main/java/com/example/teayudaapp/registerscreen/presentation/components/RegisterLoginButtons.kt
+++ app/src/main/java/com/example/teayudaapp/registerscreen/presentation/components/RegisterLoginButtons.kt
@@ -1,14 +1,20 @@
 package com.example.teayudaapp.registerscreen.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.teayudaapp.R
 
 @Composable
 fun RegisterLoginButtons(
@@ -27,30 +33,49 @@ fun RegisterLoginButtons(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.End
         ) {
-            Button(
-                onClick = { onButtonClicked(false) },
-                shape = MaterialTheme.shapes.medium,
-            ) {
+
+            Box(
+                modifier = Modifier.clickable { onButtonClicked(false) },
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.register_button),
+                    contentDescription = "Register side",
+                    tint = MaterialTheme.colors.primary,
+                    modifier = Modifier.size(width = 80.dp, height = 31.dp)
+                )
                 Text(
                     text = "Registrar",
                     style = MaterialTheme.typography.body2,
-                    color = if (!isLogin) MaterialTheme.colors.onSecondary else Color.White)
+                    color = if (!isLogin) MaterialTheme.colors.onSecondary else MaterialTheme.colors.primaryVariant
+                )
             }
-            Divider(
-                modifier = Modifier
-                    .height(30.dp)
-                    .width(1.dp)
-                    .background(Color.White)
-            )
-            Button(
-                onClick = { onButtonClicked(true) },
-                shape = MaterialTheme.shapes.medium
-            ) {
+
+            Box(
+                modifier = Modifier.clickable { onButtonClicked(true) },
+                contentAlignment = Alignment.CenterStart
+            ){
+                Icon(
+                    painter = painterResource(id = R.drawable.register_button),
+                    contentDescription = "Loguin side",
+                    tint = MaterialTheme.colors.background,
+                    modifier = Modifier
+                        .size(width = 80.dp, height = 31.dp)
+                        .rotate(180f)
+                        .offset(x = 21.dp, y = 0.dp)
+                )
                 Text(
                     text = "Login",
                     style = MaterialTheme.typography.body2,
-                    color = if (isLogin) MaterialTheme.colors.onSecondary else Color.White)
+                    color = if (isLogin) MaterialTheme.colors.onSecondary else MaterialTheme.colors.primaryVariant
+                )
             }
         }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun RegisterLoginButtonPreview(){
+    RegisterLoginButtons(padding = 0.dp, isLogin = true, onButtonClicked = {})
 }
