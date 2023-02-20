e33080b39b96677da4b235df6ce64a4310954591
Cristian Gabriel <cristian_granero90@yahoo.com>
2023-02-20 15:39:53 -0300

    Profile name, biography and created date added. View model added and a state of screen

--- app/src/main/java/com/example/teayudaapp/profilescreen/ProfileScreen.kt
+++ app/src/main/java/com/example/teayudaapp/profilescreen/ProfileScreen.kt
@@ -6,17 +6,26 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.teayudaapp.profilescreen.components.ProfileBiography
 import com.example.teayudaapp.profilescreen.components.ProfileImage
 import com.example.teayudaapp.profilescreen.components.ProfileTopBar
 
 @Composable
 fun ProfileScreen(
-    bottomNav: @Composable () -> Unit
+    bottomNav: @Composable () -> Unit,
+    viewModel: ProfileViewModel = hiltViewModel(),
 ) {
 
     val scaffoldState = rememberScaffoldState()
+    val biography = remember {
+        mutableStateOf(viewModel.state.profileBio)
+    }
     
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -29,10 +38,25 @@ fun ProfileScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding(),
+                    start = 20.dp, end = 20.dp
+                )
         ){
             item {
                 ProfileImage(name = "Profile Name", imageUrl = "https://via.placeholder.com/1000")
+            }
+            item {
+                ProfileBiography(
+                    biography = biography ,
+                    isEditable = !viewModel.state.isEditable,
+                    createdDate = viewModel.state.profileDate
+                )
+            }
+
+            items(10){
+
             }
         }
     }
--- app/src/main/java/com/example/teayudaapp/profilescreen/components/ProfileImage.kt
+++ app/src/main/java/com/example/teayudaapp/profilescreen/components/ProfileImage.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -29,22 +30,26 @@ fun ProfileImage(
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = name, style = MaterialTheme.typography.body1, color = Color.Black )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body1,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
 
             AsyncImage(
                 model = imageUrl,
                 contentDescription = "Profile image",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(146.dp).clip(shape = CircleShape),
+                modifier = Modifier
+                    .size(146.dp)
+                    .clip(shape = CircleShape),
                 error = painterResource(id = com.example.teayudaapp.R.drawable.home_material)
             )
+            Spacer(modifier = Modifier.height(20.dp))
         }
-        
     }
-
-
-
-
 }
 
 
